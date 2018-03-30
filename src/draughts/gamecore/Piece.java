852eb930579369b637dc0eb6b41420ee71573a59
James Thomas <codeactor@protonmail.com>
2018-03-30 13:19:26 +0100

    add piece class

---
+++ src/draughts/gamecore/Piece.java
@@ -0,0 +1,62 @@
+package draughts.gamecore;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+class Piece {
+    private PieceType pieceType;
+    private final BooleanProperty isKing;
+    private final ObjectProperty<PieceColor> color;
+
+    Piece(PieceType pieceType) {
+        this.pieceType = pieceType;
+        isKing = new SimpleBooleanProperty(false);
+        color = new SimpleObjectProperty<>(pieceType.color());
+    }
+
+    public PieceType getPieceType() {
+        return pieceType;
+    }
+
+    public void setPieceType(PieceType pieceType) {
+        this.pieceType = pieceType;
+
+        switch (pieceType) {
+            case WHITE_KING:
+            case BLACK_KING:
+                setIsKing(true);
+                break;
+            case NONE:
+                setColor(PieceColor.NONE);
+                setIsKing(false);
+                break;
+        }
+    }
+
+    public BooleanProperty isKingProperty() {
+        return isKing;
+    }
+
+    public boolean getIsKing() {
+        return isKing.get();
+    }
+
+    private void setIsKing(boolean b) {
+        isKing.set(b);
+    }
+
+    public ObjectProperty<PieceColor> colorProperty() {
+        return color;
+    }
+
+    public PieceColor getColor() {
+        return color.get();
+    }
+
+    private void setColor(PieceColor pieceColor) {
+        color.set(pieceColor);
+    }
+}
+
