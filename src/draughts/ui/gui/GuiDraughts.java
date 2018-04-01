4dfbf00a0c99bc71582b2814c90d279a68854f7d
James Thomas <codeactor@protonmail.com>
2018-04-01 09:22:10 +0100

    split creation of boardView from the final scene display

--- src/draughts/ui/gui/GuiDraughts.java
+++ src/draughts/ui/gui/GuiDraughts.java
@@ -1,6 +1,7 @@
 package draughts.ui.gui;
 
 import javafx.application.Application;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -13,7 +14,10 @@ public class GuiDraughts extends Application {
         final int SCENE_WIDTH = 1500;
         final int SCENE_HEIGHT = 1200;
 
-        Scene scene = new Scene(new GuiController().getBoardView(), SCENE_WIDTH, SCENE_HEIGHT, Color.LIGHTSLATEGREY);
+        GridPane boardView = new GuiController().getBoardView();
+
+        Scene scene = new Scene(boardView, SCENE_WIDTH, SCENE_HEIGHT, Color.LIGHTSLATEGREY);
+
         primaryStage.setResizable(false);
         primaryStage.setTitle("Draughts");
         primaryStage.setScene(scene);
