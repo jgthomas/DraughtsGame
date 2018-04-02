7d91b3d986625336433be349cfe75e7fabb363a6
James Thomas <codeactor@protonmail.com>
2018-04-02 08:00:24 +0100

    changed option view dimensions

--- src/draughts/ui/gui/GuiDraughts.java
+++ src/draughts/ui/gui/GuiDraughts.java
@@ -9,8 +9,8 @@ public class GuiDraughts extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        final int SCENE_WIDTH = 800;
-        final int SCENE_HEIGHT = 800;
+        final int SCENE_WIDTH = 600;
+        final int SCENE_HEIGHT = 200;
 
         Scene scene = new Scene(new OptionsController(primaryStage).getOptionsView(), SCENE_WIDTH, SCENE_HEIGHT);
 
