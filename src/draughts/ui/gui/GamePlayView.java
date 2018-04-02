749649213bdb41153dc69ef514f61cb2bf41b820
James Thomas <codeactor@protonmail.com>
2018-04-02 08:33:18 +0100

    control bar added

--- src/draughts/ui/gui/GamePlayView.java
+++ src/draughts/ui/gui/GamePlayView.java
@@ -1,11 +1,14 @@
 package draughts.ui.gui;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 public class GamePlayView {
     private final Stage primaryStage;
@@ -19,12 +22,16 @@ public class GamePlayView {
 
     public void getGamePlayView() {
         final String title = "Draughts";
-        final int SCENE_WIDTH = 1500;
-        final int SCENE_HEIGHT = 1200;
+        final int SCENE_WIDTH = 1200;
+        final int SCENE_HEIGHT = 1300;
 
         GridPane boardView = boardController.getBoardView();
-        Text titleNode = makeTitle(title);
-        boardView.add(titleNode, 10, 1);
+
+        HBox controlBar = makeControlBar();
+        controlBar.getChildren().add(makeTitle(title));
+
+        boardView.add(controlBar, 0, 10);
+
         Scene scene = new Scene(boardView, SCENE_WIDTH, SCENE_HEIGHT, Color.LIGHTSLATEGREY);
         primaryStage.setScene(scene);
     }
@@ -34,6 +41,14 @@ public class GamePlayView {
         titleNode.setText(title);
         titleNode.setFont(new Font(20));
         return titleNode;
+    }
+
+    private HBox makeControlBar() {
+        HBox controlBar = new HBox();
+        controlBar.setPrefHeight(100);
+        controlBar.setPrefWidth(1200);
+        controlBar.setAlignment(Pos.CENTER);
+        return controlBar;
     }
 
 }
