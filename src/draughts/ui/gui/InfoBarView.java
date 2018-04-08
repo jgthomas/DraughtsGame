e6bd35c41b5c60d66b75718892d607fe23e3cdf9
James Thomas <codeactor@protonmail.com>
2018-04-08 08:02:26 +0100

    added info bar to UI

---
+++ src/draughts/ui/gui/InfoBarView.java
@@ -0,0 +1,11 @@
+package draughts.ui.gui;
+
+import javafx.scene.layout.VBox;
+
+class InfoBarView extends VBox {
+
+    InfoBarView() {
+        this.setPrefWidth(150);
+        this.setPrefHeight(800);
+    }
+}
