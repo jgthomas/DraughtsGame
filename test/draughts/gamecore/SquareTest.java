178d957bec9ba8e1da4b96be1c8bd4e929188b0b
James Thomas <codeactor@protonmail.com>
2018-04-12 21:03:13 +0100

    add initial test class

---
+++ test/draughts/gamecore/SquareTest.java
@@ -0,0 +1,28 @@
+package draughts.gamecore;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SquareTest {
+
+    @Test
+    public void rowHigher() {
+    }
+
+    @Test
+    public void colHigher() {
+    }
+
+    @Test
+    public void isLegalMoveDistance() {
+    }
+
+    @Test
+    public void isLegalTakeDistance() {
+    }
+
+    @Test
+    public void isWithinRange() {
+    }
+}
