265ff86cdd869a66bb50bb53696412f794c85eac
James Thomas <codeactor@protonmail.com>
2018-04-12 18:31:59 +0100

    removed repetitive wrapper methods in favour of direct calls to the ones doing the work

--- src/draughts/ui/tui/UserInput.java
+++ src/draughts/ui/tui/UserInput.java
@@ -3,7 +3,7 @@ package draughts.ui.tui;
 
 import java.util.Scanner;
 
-public final class UserInput {
+final class UserInput {
 
     private final String PLAYER_PICK_MESSAGE = String.join(System.getProperty("line.separator"),
             "(1) Human",
@@ -13,34 +13,6 @@ public final class UserInput {
 
     private final Scanner scanner = new Scanner(System.in);
 
-    boolean playSavedGame() {
-        return getYesOrNo("Load saved game [y/n]: ");
-    }
-
-    boolean seeNextMove() {
-        return getYesOrNo("See next move [y/n]: ");
-    }
-
-    boolean saveGame() {
-        return getYesOrNo("Save game [y/n]: ");
-    }
-
-    String setGameName() {
-        return getString("Give game a name: ");
-    }
-
-    public String getPlayerName() {
-        return getString("Pick your player name: ");
-    }
-
-    int selectSavedGame() {
-        return getNumber("Pick game number to load: ");
-    }
-
-    public int selectSavedMove(int lastMoveNumber) {
-        return getNumberInRange(0, lastMoveNumber,"Pick the move to load: ");
-    }
-
     int pickPlayerType(String message) {
         String fullMessage = String.join(System.getProperty("line.separator"),
                 message,
@@ -49,34 +21,40 @@ public final class UserInput {
         return getNumber(fullMessage);
     }
 
-    private boolean getYesOrNo(String message) {
-        System.out.print(message);
+    boolean advanceForward(String message) {
+        System.out.print(message + " (or enter 'y' to accept): ");
+        String answer = scanner.nextLine();
+        return answer.isEmpty();
+    }
+
+    boolean getYesOrNo(String message) {
+        System.out.print(message + " [y/n]: ");
         String answer = scanner.next();
         char response = Character.toLowerCase(answer.charAt(0));
         return response == 'y';
     }
 
-    private int getNumber(String message) {
-        System.out.print(message);
+    int getNumber(String message) {
+        System.out.print(message + " : ");
         while (!scanner.hasNextInt()) {
-            System.out.print(message);
+            System.out.print(message + " : ");
             scanner.next();
         }
         return scanner.nextInt();
     }
 
-    private int getNumberInRange(int low, int high, String message) {
+    int getNumberInRange(int low, int high, String message) {
         int num = 0;
         while (num < low || num > high) {
-            num = getNumber(message);
+            num = getNumber(message + ": ");
         }
         return num;
     }
 
-    private String getString(String message) {
+    String getString(String message) {
         String str = "";
         while (str.length() < 1) {
-            System.out.print(message);
+            System.out.print(message + ": ");
             str = scanner.next();
         }
         return str;
