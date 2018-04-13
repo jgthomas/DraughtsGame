ee536f5857881d521b074426f3f0f216e7e5bc76
James Thomas <codeactor@protonmail.com>
2018-04-13 16:52:28 +0100

    add two methods to the interface

--- src/draughts/gamecore/BoardStateLoader.java
+++ src/draughts/gamecore/BoardStateLoader.java
@@ -1,7 +1,13 @@
 package draughts.gamecore;
 
+import java.util.List;
+
 public interface BoardStateLoader {
 
     PieceType getPieceType(Square square);
+
+    List<Square> squares();
+
+    int getBoardSize();
 
 }
