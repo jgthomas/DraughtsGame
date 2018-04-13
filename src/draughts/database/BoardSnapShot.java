00f9388b84ac5fa7eeba18933a272eeade70b8ec
James Thomas <codeactor@protonmail.com>
2018-04-13 16:52:16 +0100

    add two methods to generate default board layout

--- src/draughts/database/BoardSnapShot.java
+++ src/draughts/database/BoardSnapShot.java
@@ -5,10 +5,13 @@ import draughts.gamecore.BoardStateLoader;
 import draughts.gamecore.PieceType;
 import draughts.gamecore.Square;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
 public class BoardSnapShot implements BoardStateLoader {
+    private final int BOARD_SIZE = 8;
     private final Map<Integer, Integer> boardSnapShot;
 
     BoardSnapShot(Board board) {
@@ -24,5 +27,19 @@ public class BoardSnapShot implements BoardStateLoader {
 
     public PieceType getPieceType(Square square) {
         return PieceType.valueOf(boardSnapShot.get(square.hashCode()));
+    }
+
+    public List<Square> squares() {
+        List<Square> squares = new ArrayList<>();
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                squares.add(new Square(row, col));
+            }
+        }
+        return squares;
+    }
+
+    public int getBoardSize() {
+        return BOARD_SIZE;
     }
 }
