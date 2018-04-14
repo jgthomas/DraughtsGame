b120367102c2fec72ce5174aba8c071d4ecd236f
James Thomas <codeactor@protonmail.com>
2018-04-14 14:08:58 +0100

    replaced basic move-type getter with method that checks if move is take

--- src/draughts/gamecore/Move.java
+++ src/draughts/gamecore/Move.java
@@ -51,8 +51,8 @@ public class Move implements Comparable<Move>, Iterable<Move> {
         return pieceType;
     }
 
-    MoveType type() {
-        return moveType;
+    boolean isTake() {
+        return moveType == MoveType.TAKE;
     }
 
     void setNextTake(Move move) {
