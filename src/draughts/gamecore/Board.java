5f441c39cecf1e430d50c7b1f060f8d4ae7350ec
James Thomas <codeactor@protonmail.com>
2018-04-15 18:30:15 +0100

    information required to make new board back in the board class

--- src/draughts/gamecore/Board.java
+++ src/draughts/gamecore/Board.java
@@ -1,22 +1,28 @@
 package draughts.gamecore;
 
-import java.util.Iterator;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Board implements Iterable<Square> {
     private final SortedMap<Square, Piece> boardMap = new TreeMap<>();
-    private final int boardSize;
+    private final int BOARD_SIZE = 8;
+
+    public Board() {
+        BoardInit boardInit = new BoardInit();
+        for (Square square : squares()) {
+            boardMap.put(square, new Piece(PieceType.valueOf(boardInit.getPieceCode(square))));
+        }
+    }
 
     public Board(BoardStateLoader boardStateLoader) {
-        boardSize = boardStateLoader.getBoardSize();
-        for (Square square : boardStateLoader.squares()) {
+        for (Square square : squares()) {
             boardMap.put(square, new Piece(PieceType.valueOf(boardStateLoader.getPieceCode(square))));
         }
     }
 
     final void setBoardState(BoardStateLoader boardStateLoader) {
-        for (Square square : boardStateLoader.squares()) {
+        for (Square square : squares()) {
             setPieceType(square, PieceType.valueOf(boardStateLoader.getPieceCode(square)));
         }
     }
@@ -48,7 +54,7 @@ public class Board implements Iterable<Square> {
     }
 
     public int sideLength() {
-        return boardSize;
+        return BOARD_SIZE;
     }
 
     @Override
@@ -58,5 +64,50 @@ public class Board implements Iterable<Square> {
 
     private void setPieceType(Square square, PieceType pieceType) {
         boardMap.get(square).setPieceType(pieceType);
+    }
+
+    private List<Square> squares() {
+        List<Square> squares = new ArrayList<>();
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                squares.add(new Square(row, col));
+            }
+        }
+        return squares;
+    }
+
+    private class BoardInit {
+        private final Map<Integer, Integer> initialState;
+
+        BoardInit() {
+            initialState = zipToMap(squareHashes(), pieceTypeCodes());
+        }
+
+        private int getPieceCode(Square square) {
+            return initialState.get(square.hashCode());
+        }
+
+        private List<Integer> squareHashes() {
+            List<Integer> squareHash = new ArrayList<>();
+            for (Square square : squares()) {
+                squareHash.add(square.hashCode());
+            }
+            return squareHash;
+        }
+
+        private List<Integer> pieceTypeCodes() {
+            final int NUM_OF_SQUARES = BOARD_SIZE * BOARD_SIZE;
+            List<Integer> codes = new ArrayList<>(Collections.nCopies(NUM_OF_SQUARES, 0));
+            List<Integer> whitePositions = new ArrayList<>(Arrays.asList(0,2,4,6,9,11,13,15,16,18,20,22));
+            List<Integer> blackPositions = new ArrayList<>(Arrays.asList(41,43,45,47,48,50,52,54,57,59,61,63));
+            for (Integer whitePos : whitePositions) { codes.add(whitePos, PieceType.WHITE_PIECE.value()); }
+            for (Integer blackPos : blackPositions) { codes.add(blackPos, PieceType.BLACK_PIECE.value()); }
+            return codes;
+        }
+
+        private <K, V> Map<K, V> zipToMap(List<K> keys, List<V> values) {
+            return IntStream.range(0, keys.size()).boxed()
+                    .collect(Collectors.toMap(keys::get, values::get));
+        }
     }
 }
