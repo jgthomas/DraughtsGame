78e5b23993bcf5addd72876e2510878c3c99377e
James Thomas <codeactor@protonmail.com>
2018-04-16 23:01:45 +0100

    add first test for LegalMoves

---
+++ test/draughts/gamecore/LegalMovesTest.java
@@ -0,0 +1,44 @@
+package draughts.gamecore;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LegalMovesTest {
+    private final Board board = new Board();
+    private final LegalMoves legalMoves = new LegalMoves(board);
+
+    @Test
+    public void legal() {
+    }
+
+    @Test
+    public void legalStartingSquares() {
+        List<Square> legalStartsBlack = new ArrayList<>();
+        legalStartsBlack.add(new Square(2,1));
+        legalStartsBlack.add(new Square(2,1));
+        legalStartsBlack.add(new Square(2,3));
+        legalStartsBlack.add(new Square(2,3));
+        legalStartsBlack.add(new Square(2,5));
+        legalStartsBlack.add(new Square(2,5));
+        legalStartsBlack.add(new Square(2,7));
+        Assert.assertArrayEquals(legalStartsBlack.toArray(), legalMoves.legalStartingSquares(Side.BLACK).toArray());
+        List<Square> legalStartsWhite = new ArrayList<>();
+        legalStartsWhite.add(new Square(5,0));
+        legalStartsWhite.add(new Square(5,2));
+        legalStartsWhite.add(new Square(5,2));
+        legalStartsWhite.add(new Square(5,4));
+        legalStartsWhite.add(new Square(5,4));
+        legalStartsWhite.add(new Square(5,6));
+        legalStartsWhite.add(new Square(5,6));
+        Assert.assertArrayEquals(legalStartsWhite.toArray(), legalMoves.legalStartingSquares(Side.WHITE).toArray());
+    }
+
+    @Test
+    public void legalEndingSquares() {
+    }
+}
