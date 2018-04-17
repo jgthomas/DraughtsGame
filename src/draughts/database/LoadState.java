ae2d233e6ce2c9c1d4223460ff35c7255a812c65
James Thomas <codeactor@protonmail.com>
2018-04-17 11:25:21 +0100

    add two private methods for caching state of whole game from database

--- src/draughts/database/LoadState.java
+++ src/draughts/database/LoadState.java
@@ -1,11 +1,14 @@
 package draughts.database;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LoadState {
 
     private final DB db = new DB();
+    private Map<Integer, Map<Integer, Integer>> cachedState;
 
     public List<String> getAllGameNames() {
         List<String> gameNames =  db.selectAllGameNames();
@@ -25,5 +28,17 @@ public class LoadState {
 
     public int totalMoves(String gameName) {
         return db.totalMovesInGame(gameName);
+    }
+
+    private Map<Integer, Map<Integer, Integer>> loadGameToCache(String gameName, int turnNumber) {
+        Map<Integer, Map<Integer, Integer>> cachedState = new HashMap<>();
+        for (int turn = 0; turn < totalMoves(gameName); turn++) {
+            cachedState.put(turn, getTurnState(gameName, turn));
+        }
+        return cachedState;
+    }
+
+    private Map<Integer, Integer> getTurnState(String gameName, int turnNumber) {
+        return db.selectGame(gameName, turnNumber);
     }
 }
