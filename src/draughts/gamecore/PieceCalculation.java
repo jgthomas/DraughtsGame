0481553a955a4a5d9bf5699f6eefa646621b3176
James Thomas <codeactor@protonmail.com>
2018-04-17 17:15:26 +0100

    down to two methods in interface

--- src/draughts/gamecore/PieceCalculation.java
+++ src/draughts/gamecore/PieceCalculation.java
@@ -2,10 +2,6 @@ package draughts.gamecore;
 
 public interface PieceCalculation {
 
-    String pieceString();
-
-    String shortPieceString();
-
     boolean legalMoveDirection(Square start, Square end);
 
     int takenRow(Square start, Square end);
