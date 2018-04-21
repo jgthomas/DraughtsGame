5bedef3914f7df2089d747c5a4c062b07834a010
James Thomas <codeactor@protonmail.com>
2018-04-21 08:24:52 +0100

    improved formatting of help message

--- src/draughts/ui/tui/CommandRunner.java
+++ src/draughts/ui/tui/CommandRunner.java
@@ -13,6 +13,7 @@ class CommandRunner {
         commandMap.put("forw", gameController::forwardOneMove);
         commandMap.put("play", gameController::aiResume);
         commandMap.put("rese", gameController::restartGame);
+        commandMap.put("begi", gameController::newGame);
         commandMap.put("clea", gameController::displayBoard);
         commandMap.put("quit", this::quit);
     }
@@ -27,14 +28,19 @@ class CommandRunner {
     }
 
     private void help() {
+        System.out.println();
+        System.out.println("Game commands");
+        System.out.println("#############");
         System.out.println("help    : print this message");
         System.out.println("save    : save game");
         System.out.println("back    : move back one move");
         System.out.println("forward : move forward one move");
         System.out.println("play    : make computer player resume");
-        System.out.println("restart : reset game to initial state");
+        System.out.println("reset   : reset game to initial state");
+        System.out.println("begin   : begin new game");
         System.out.println("clear   : clear all command text");
         System.out.println("quit    : exit the game");
+        System.out.println();
     }
 
     private void quit() {
